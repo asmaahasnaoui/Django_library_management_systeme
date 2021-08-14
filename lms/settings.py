45d0aa4c647d8b79b5b771b05bcc645be9f0df44
Asmaa Hasnaoui <asmahasnaoui998@gmail.com>
2021-08-14 18:38:58 +0100

    first init

--- lms/settings.py
+++ lms/settings.py
@@ -125,8 +125,12 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'lms/static')
+    os.path.join(BASE_DIR, 'lms/static'),
 ]
+
+#media files
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
